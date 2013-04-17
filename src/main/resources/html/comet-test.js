3600052f6e45724c425409dd6bb80cb7638a4469
Adrian Milne <adrian@corsoft.co.uk>
2013-04-17 21:28:38 +0100

    Development update

--- src/main/resources/html/comet-test.js
+++ src/main/resources/html/comet-test.js
@@ -1,19 +1,24 @@
 
 google.load("jquery", "1");
 
+/**
+ * Callback function - Cometd consumer.
+ */
 google.setOnLoadCallback(function() {
-  $.getScript("http://jquerycomet.googlecode.com/svn/trunk/jquery.comet.js", function(){
+    $.getScript("http://jquerycomet.googlecode.com/svn/trunk/jquery.comet.js", function(){
     console.log("done loading js");
     $.comet.init("http://localhost:9099/cometd");
-    $.comet.subscribe("/broadcastMessageChannel", receive);
+    $.comet.subscribe("/broadcastMessageChannel", broadcastListener);
   });
   
 });
 
-function receive(message) {
-  console.log("message: " + message + ", " + message.data + ", header: " + message.header );
-  
-  $('<li>').html(message.data).prependTo('#contentList').animate({color: "#000000"}, 6000);
+/**
+ * Listener function called on receipt of broadcast message.
+ */
+function broadcastListener(msg) {
+  console.log("received broadcast: " + msg + ", " + msg.data); 
+  $('<li>').html(msg.data).prependTo('#contentList').animate({color: "#000000"}, 6000);
   $('html, body').animate({ scrollTop: 0 }, 0);
 }
 
